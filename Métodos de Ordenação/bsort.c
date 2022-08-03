d819449ca24ddb92d3caf1dd2501b5ab1500ada9
Dayane Núbia <dayane.nubia67@gmail.com>
2022-08-03 20:49:54 -0300

    Update bsort.c

--- Métodos de Ordenação/bsort.c
+++ Métodos de Ordenação/bsort.c
@@ -2,12 +2,29 @@
 #include <stdlib.h>
 #include "biblioteca.h"
 
+//void bsort(int v[], int n){
+//	int i,j;
+//	for(i=1; i<n; i++){
+//		for(j=0; j<n-i; j++){
+//			if(v[j] > v[j+1]){
+//				troca(&v[j], &v[j+1]);
+//			}
+//		}
+//	}
+//}
+
 void bsort(int v[], int n){
 	int i,j;
-	for(i=1; i<n; i++){
-		for(j=0; j<n-i; j++){
+	for(i=1; i<n; i++){//fases
+		printf("Estou na fase %d\n", i); getch();
+		printVetor(v,n); //passo a passo 
+		for(j=0; j<n-i; j++){//comparações
+			printf("%d > %d\n", v[j], v[j+1]); getch();
+			comp++;
 			if(v[j] > v[j+1]){
+				printf("Trocando %d com %d", v[j], v[j+1]); 
 				troca(&v[j], &v[j+1]);
+				printVetor(v,n); getch();
 			}
 		}
 	}
